cd05bca16aa70fbdfcfe4f1ab9df94c6fd8846d7
Jing Wait <jingzhaowait@gmail.com>
2015-11-03 15:20:28 -0800

    add new dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,7 +1,17 @@
 class DogsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @dogs = @user.dogs
+  end
+
   def new
     @user = User.find(params[:user_id])
     @dog = @user.dogs.new
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @dog = @user.dogs.find(params[:id])
   end
 
   def create
@@ -9,9 +19,28 @@ class DogsController < ApplicationController
     @dog = @user.dogs.new(dog_params)
 
     @dog.save
-    redirect_to user_path(@user)
+    redirect_to user_dogs_path(@user, @dog)
 
   end
+
+
+
+  def update
+    @user = User.find(params[:user_id])
+    @dog = @user.dogs.find(params[:id])
+    @dog.update(dog_params)
+    redirect_to user_dog_path(@user)
+
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @dog = @user.dogs.find(params[:id])
+    @dog.destroy
+    redirect_to  user_dogs_path(@user)
+
+  end
+
 
   private
     def dog_params
